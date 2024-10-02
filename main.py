a5dd53b8cf0f16b31327a6b0343e8d27a125fef6
queuegeforcenow <queuegeforcenow@gmail.com>
2024-10-02 14:53:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ログインしました！")
+    await bot.change_presence(activity=discord.Game(name="1"))
+
+bot.run("MTIzMjQ4MjU5NTI0NjMxMzUyNA.GsR1MR.1Y6Ij89xEIij2_hakkEhqCSe4G2JwzxeAtYvho")
